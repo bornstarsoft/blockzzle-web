aac9b0fc95d5a28a46fecbb83f959d5bc513d33a
Seong Jin Kim <bornstarsoft@gmail.com>
2026-06-07 14:08:06 +0900

    Fix Blockzzle mobile viewport layout

--- tests/blockzzle_logic.test.cjs
+++ tests/blockzzle_logic.test.cjs
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const { BlockzzleCore } = require("../static/play/js/blockzzle-phaser.v002.js");
+const { BlockzzleCore } = require("../static/play/js/blockzzle-phaser.v003.js");
 
 function piece(cells, id = "test") {
   return { id, cells, color: 0 };
